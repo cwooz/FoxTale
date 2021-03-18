6276c17898084ff67b1f6a3c6d8b17d086d6b8d4
Chris Woosley <woosley.christopher@gmail.com>
2021-03-17 22:02:16 -0400

    Added Camera Parallax for depth

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,26 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
 
+    public Transform farBackground, middleBackground;
+
+    private float lastXPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastXPosition = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);    // Follow Player
+
+        float amountToMoveX = transform.position.x - lastXPosition;     // Calculate how far camera has moved
+
+        farBackground.position += new Vector3(amountToMoveX, 0f, 0f);
+        middleBackground.position += new Vector3(amountToMoveX * .5f, 0f, 0f);
+
+        lastXPosition = transform.position.x;
     }
 }
